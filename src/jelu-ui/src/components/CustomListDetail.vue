55574b1869b83db3b2175502526df13f7c2f3583
Abdulelah444 <49835961+Abdulelah444@users.noreply.github.com>
2026-05-17 09:29:33 +0300

    Adjust grid layout for CustomListDetail component

--- src/jelu-ui/src/components/CustomListDetail.vue
+++ src/jelu-ui/src/components/CustomListDetail.vue
@@ -145,7 +145,7 @@ getList()
   </h2>
   <div
     v-if="books.length > 0"
-    class="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8 gap-0 my-3 shrink-0 grow-0 mt-2"
+    class="grid grid-cols-3 md:grid-cols-5 xl:grid-cols-7 2xl:grid-cols-9 gap-0 my-3 shrink-0 grow-0 mt-2"
   >
     <div
       v-for="book in convertedBooks"
