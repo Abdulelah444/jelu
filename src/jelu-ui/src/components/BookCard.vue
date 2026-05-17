6b35a1d4d143de5478786ba5ce3992c42603db3c
Abdulelah444 <49835961+Abdulelah444@users.noreply.github.com>
2026-05-17 09:13:35 +0300

    Change image class to 'object-contain' in BookCard
    Updated image classes to use 'object-contain' for better aspect ratio handling.

--- src/jelu-ui/src/components/BookCard.vue
+++ src/jelu-ui/src/components/BookCard.vue
@@ -120,14 +120,14 @@ const currentTimestamp = ObjectUtils.timestamp()
             v-if="book.book.image"
             :src="'/files/' + book.book.image + '?timestamp=' + currentTimestamp"
             alt="cover image"
-            class="object-cover w-full"
+            class="object-contain"
             :class="props.size === 'xl' ? 'h-80' : 'h-56'"
           >
           <img
             v-else
             src="../assets/placeholder_asset.jpg"
             alt="cover placeholder"
-            class="h-56 object-cover w-full"
+            class="h-56 object-contain"
           >
         </figure>
       </router-link>
@@ -147,7 +147,7 @@ const currentTimestamp = ObjectUtils.timestamp()
             v-else
             src="../assets/placeholder_asset.jpg"
             alt="cover placeholder"
-            class="h-56 object-cover w-full"
+            class="h-56 object-contain"
           >
         </figure>
       </router-link>
