1866ade63f556927bfc286efce398a54f3f2869b
Abdulelah444 <49835961+Abdulelah444@users.noreply.github.com>
2026-05-17 09:29:56 +0300

    Adjust grid layout for AuthorsList component

--- src/jelu-ui/src/components/AuthorsList.vue
+++ src/jelu-ui/src/components/AuthorsList.vue
@@ -251,7 +251,7 @@ try {
   />
   <div
     v-if="authors.length > 0"
-    class="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8 gap-0 my-3 shrink-0 grow-0 mt-2"
+    class="grid grid-cols-3 md:grid-cols-5 xl:grid-cols-7 2xl:grid-cols-9 gap-0 my-3 shrink-0 grow-0 mt-2"
   >
     <TransitionGroup name="list">
       <div
